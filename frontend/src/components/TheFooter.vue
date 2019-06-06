f3a1b29e07b7458b29a2ab9b91f87cd645d97cd3
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-06 11:56:14 -0400

    Add landing page footer and header for usable interface

---
+++ frontend/src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }">
+        Insurance - Magic
+      </router-link>
+      <span class="attribution">
+        Your favorite place for your favorite things
+        <a rel="noopener noreferrer" target="blank"
+          ></a>
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "RwvFooter"
+};
+</script>
