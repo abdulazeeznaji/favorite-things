5f75d6bccb473213919303894192a8199c2adf7b
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-06 12:11:39 -0400

    Plug vueJS router for smooth and SPA experience

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <RwvHeader />
+    <router-view></router-view>
+    <RwvFooter />
+  </div>
+</template>
+
+<script>
+import RwvHeader from "@/components/TheHeader";
+import RwvFooter from "@/components/TheFooter";
+export default {
+  name: "App",
+  components: {
+    RwvHeader,
+    RwvFooter
+  }
+};
+</script>
+
+<style></style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">  Favirate </h1>
+        <p> Track what you want </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RwvTag from "@/components/VTag";
+export default {
+  name: "home",
+  components: {
+    RwvTag
+  }
+};
+</script>
