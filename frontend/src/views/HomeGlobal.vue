0979d6fcbae5e2d1b51aa1feb362a2da85a4c201
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-10 13:50:49 -0400

    Create global favorite feed

---
+++ frontend/src/views/HomeGlobal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home-global"><RwvArticleList type="all" /></div>
+</template>
+<script>
+import RwvArticleList from "@/components/FavoritesList.vue";
+
+export default {
+  name: "rwv-home-global",
+  components: {
+    RwvArticleList
+  }
+};
+</script>
