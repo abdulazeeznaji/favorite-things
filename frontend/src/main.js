e9956cd3de67536215d1aae297c8aab4e69d7c1a
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-10 13:53:41 -0400

    Plug our store to vue js application

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
+import ApiService from "./common/api.service";
 
+ApiService.init();
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
