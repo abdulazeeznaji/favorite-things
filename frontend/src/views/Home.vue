ef63ae974636f968b70e5bbc50d4a191a9c615be
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-10 13:52:21 -0400

    Home page for favorites list

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,15 +2,33 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">  Favirate </h1>
-        <p> Track what you want </p>
+        <h1 class="logo-font"> Favorite </h1>
+        <p>Track your favorites .</p>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+              <router-link
+                  :to="{ name: 'home' }"
+                  exact
+                  class="nav-link"
+                  active-class="active"
+                >
+                  Your favorite list
+                </router-link>
+          </div>
+          <router-view></router-view>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
 
-}
+export default {
+  name: "home",
+};
 </script>
