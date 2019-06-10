668a650376fe97c111436657d0f3e298591be2ae
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-10 13:51:26 -0400

    Preview each favorite that has been added

---
+++ frontend/src/components/VArticlePreview.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="article-preview">
+    <RwvArticleMeta isPreview :article="article" />
+    <router-link :to="articleLink" class="preview-link">
+      <h1 v-text="article.title" />
+      <p v-text="article.description" />
+      <span>Read more...</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "RwvArticlePreview",
+  components: {
+  },
+  props: {
+    article: { type: Object, required: true }
+  },
+  computed: {
+    articleLink() {
+      return {
+        name: "article",
+        params: {
+          slug: this.article.slug
+        }
+      };
+    }
+  }
+};
+</script>
