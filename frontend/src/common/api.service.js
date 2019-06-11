f24d03354ab7f1a2209ff8c39bce47f8dd9ef696
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-11 12:36:24 -0400

    Add post method

--- frontend/src/common/api.service.js
+++ frontend/src/common/api.service.js
@@ -4,34 +4,41 @@ import VueAxios from "vue-axios";
 import { API_URL } from "@/common/config";
 
 const ApiService = {
-  init() {
-    Vue.use(VueAxios, axios);
-    Vue.axios.defaults.baseURL = API_URL;
-  },
+    init() {
+        Vue.use(VueAxios, axios);
+        Vue.axios.defaults.baseURL = API_URL;
+    },
 
 
-  query(resource, params) {
-    return Vue.axios.get(resource, params).catch(error => {
-      throw new Error(`[RWV] ApiService ${error}`);
-    });
-  },
-  get(resource, slug = "") {
-    return Vue.axios.get(`${resource}/${slug}`).catch(error => {
-      throw new Error(`[RWV] ApiService ${error}`);
-    });
-  },
+    query(resource, params) {
+        return Vue.axios.get(resource, params).catch(error => {
+            throw new Error(`[RWV] ApiService ${error}`);
+        });
+    },
+    get(resource, slug = "") {
+        return Vue.axios.get(`${resource}/${slug}`).catch(error => {
+            throw new Error(`[RWV] ApiService ${error}`);
+        });
+    },
+    post(resource, params) {
+        return Vue.axios.post(`${resource}`, params);
+    },
+
 };
 
 
 export default ApiService;
 
 export const FavoriteService = {
-  query(type, params) {
-    return ApiService.query("favorites" + (type === "feed" ? "/feed" : ""), {
-      params: params
-    });
-  },
-  get(slug) {
-    return ApiService.get("favorites", slug);
-  },
+    query(type, params) {
+        return ApiService.query("favorites" + (type === "feed" ? "/feed" : ""), {
+            params: params
+        });
+    },
+    get(slug) {
+        return ApiService.get("favorites", slug);
+    },
+    create(params) {
+        return ApiService.post("favorites", params );
+    },
 };
