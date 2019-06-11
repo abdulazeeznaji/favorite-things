cca0a573e63c3ca6f9c76b6a60881459d8c9a0b0
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-11 12:38:43 -0400

    Add create endpoint on post/favorite and handle reorder business

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_cors import CORS
+from datetime import datetime
 
 
 application = Flask(__name__)
@@ -11,6 +12,7 @@ cors = CORS(application, resources={r"/api/*": {"origins": "*"}})
 from app.models import *
 
 
+# Fetch all favorites
 @application.route('/api/favorites', methods=['GET'])
 def api_feature_get():
     favorites_query = Favorites.query.all()
@@ -18,3 +20,31 @@ def api_feature_get():
     for i in favorites_query:
         favorites_list.append(i.serialize)
     return jsonify({'favorites': favorites_list}), 200
+
+
+# Create new favorite
+@application.route('/api/favorites', methods=['POST'])
+def api_favorite_post():
+
+    ranking = request.json.get('ranking') \
+        if request.json.get('ranking') \
+        else db.session.query(db.func.max(Favorites.ranking)).filter(Favorites.category == request.json.get('category')).scalar() + 1 or 1
+
+    favorites = Favorites.query.filter(Favorites.category_area_id == request.json.get('category'), Favorites.ranking >= ranking).order_by(Favorites.ranking).all()
+    i = int(ranking)
+
+    for favorite in favorites:
+        favorite.ranking = i + 1
+        i += 1
+        db.session.commit()
+    created_date = datetime.datetime.strptime(request.json.get('created_date'), '%d-%m-%Y')
+    data = Favorites(request.json.get('title'),
+                     request.json.get('description'),
+                     ranking,
+                     created_date,
+                     request.json.get('category'),
+                     )
+
+    db.session.add(data)
+    db.session.commit()
+    return jsonify({'message': 'Favorite successfully added'}), 201
