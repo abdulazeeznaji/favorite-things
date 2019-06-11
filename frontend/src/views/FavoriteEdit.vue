840cda06e9069e31f512d86ba043170f15e92f03
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-11 12:42:48 -0400

    Add form template to add new favorites

---
+++ frontend/src/views/FavoriteEdit.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="editor-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-10 offset-md-1 col-xs-12">
+          <RwvListErrors :errors="errors" />
+          <form v-on:submit.prevent="onPublish(favorite.slug);">
+            <fieldset :disabled="inProgress">
+              <fieldset class="form-group">
+                <input
+                        type="text"
+                        class="form-control form-control-lg"
+                        v-model="favorite.title"
+                        placeholder="Title"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <input
+                        type="text"
+                        class="form-control"
+                        v-model="favorite.description"
+                        placeholder="Description"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <input
+                        type="number"
+                        class="form-control"
+                        v-model="favorite.ranking"
+                        placeholder="Ranking"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <select
+                        type="text"
+                        class="form-control"
+                        v-model="favorite.category"
+                        placeholder="Category"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+                <input
+                        disabled
+                        class="form-control"
+                        v-model="favorite.created_date"
+                        placeholder="Ranking"
+                />
+              </fieldset>
+              <fieldset class="form-group">
+              </fieldset>
+            </fieldset>
+            <button
+                    :disabled="inProgress"
+                    class="btn btn-lg pull-xs-right btn-primary"
+                    type="submit"
+            >
+              Publish Favorite
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from "vuex";
+  import store from "@/store";
+  import RwvListErrors from "@/components/ListErrors";
+  import {
+    FAVORITE_PUBLISH,
+  } from "@/store/actions.type";
+
+  export default {
+    name: "RwvFavoriteEdit",
+    components: { RwvListErrors },
+    props: {
+      previousFavorite: {
+        type: Object,
+        required: false
+      }
+    },
+    data() {
+      return {
+        tagInput: null,
+        inProgress: false,
+        errors: {}
+      };
+    },
+    computed: {
+      ...mapGetters(["favorite"])
+    },
+    methods: {
+      onPublish(slug) {
+        let action = FAVORITE_PUBLISH;
+        this.inProgress = true;
+        this.$store
+                .dispatch(action)
+                .then(({ data }) => {
+                  this.inProgress = false;
+                  this.$router.push({
+                    name: "favorite",
+                    params: { slug: data.favorite.slug }
+                  });
+                })
+                .catch(({ response }) => {
+                  this.inProgress = false;
+                  this.errors = response.data.errors;
+                });
+      }
+    }
+  };
+</script>
