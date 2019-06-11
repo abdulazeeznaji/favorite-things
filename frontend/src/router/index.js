c4c012633f0576dc727b70af24d9428e8ba76343
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-11 12:46:47 -0400

    Create new favorite edit route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,16 +4,22 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [    {
-      path: "/",
-      component: () => import("@/views/Home"),
-      children: [
-        {
-          path: "",
-          name: "home",
-          component: () => import("@/views/HomeGlobal")
-        }
-      ]
+    routes: [    {
+        path: "/",
+        component: () => import("@/views/Home"),
+        children: [
+            {
+                path: "",
+                name: "home",
+                component: () => import("@/views/HomeGlobal")
+            }
+        ]
     },
-  ]
+        {
+            name: "favorite-edit",
+            path: "/editor/:slug?",
+            props: true,
+            component: () => import("@/views/FavoriteEdit")
+        }
+    ]
 })
