26ec2dad65eb5a2bf85ccfe10155e262754a9425
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-13 19:24:15 -0400

    Add FETCH_CATEGORY action

--- frontend/src/store/actions.type.js
+++ frontend/src/store/actions.type.js
@@ -2,3 +2,7 @@
 export const FETCH_FAVORITES = "fetchFavorites";
 export const FETCH_FAVORITE = "fetchFavorite";
 export const FAVORITE_PUBLISH = "publishFavorite";
+
+
+// Categories actions
+export const FETCH_CATEGORIES = "fetchCategories";
