7a34d0f3f0dfea17fdb073e528f1b5eb8f3368a2
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-13 19:27:00 -0400

    Update store when fetching categories

--- frontend/src/store/favorite.module.js
+++ frontend/src/store/favorite.module.js
@@ -1,59 +1,81 @@
 import Vue from "vue";
 import {
-  FavoriteService,
+    FavoriteService,
+    CategoriesService
 } from "@/common/api.service";
 import {
-  FETCH_FAVORITE,
-  FAVORITE_PUBLISH,
+    FETCH_FAVORITE,
+    FAVORITE_PUBLISH,
+    FETCH_CATEGORIES
 } from "./actions.type";
 import {
-  SET_FAVORITE,
+    SET_FAVORITE,
+    SET_CATEGORIES
 } from "./mutations.type";
 
 var curday = function(sp){
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //As January is 0.
-var yyyy = today.getFullYear();
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //As January is 0.
+    var yyyy = today.getFullYear();
 
-if(dd<10) dd='0'+dd;
-if(mm<10) mm='0'+mm;
-return (mm+sp+dd+sp+yyyy);
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return (mm+sp+dd+sp+yyyy);
 };
 
 const initialState = {
-  favorite: {
-    title: "",
-    description: "",
-    ranking: '',
-    category: '',
-    created_date: curday('-')
-  },
+    favorite: {
+        title: "",
+        description: "",
+        ranking: '',
+        categories: [],
+        created_date: curday('-'),
+        selected:''
+    }
 };
 
 export const state = { ...initialState };
 
 export const actions = {
-  [FAVORITE_PUBLISH]({ state }) {
-    return FavoriteService.create(state.favorite);
-  }
+    [FAVORITE_PUBLISH]({ state }) {
+        return FavoriteService.create(state.favorite);
+    },
+    [FETCH_CATEGORIES](context) {
+        return CategoriesService.get()
+            .then(({ data }) => {
+                context.commit(SET_CATEGORIES, data.categories);
+            })
+            .catch(error => {
+                throw new Error(error);
+            });
+    }
 };
 
 export const mutations = {
-  [SET_FAVORITE](state, favorite) {
-    state.favorite = favorite;
-  }
+    [SET_FAVORITE](state, favorite) {
+        state.favorite = favorite;
+    },
+    [SET_CATEGORIES](state, categories) {
+        state.favorite.categories = categories;
+    },
 };
 
 const getters = {
-  favorite(state) {
-    return state.favorite;
-  }
+    favorite(state) {
+        return state.favorite;
+    },
+    categories(state) {
+        return state.categories;
+    },
+    selected(state) {
+        return state.selected;
+    }
 };
 
 export default {
-  state,
-  actions,
-  mutations,
-  getters
+    state,
+    actions,
+    mutations,
+    getters
 };
