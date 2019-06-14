56f66de41471e958deb53d180c8875290a32efc1
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-14 02:51:55 -0400

    Update prod url

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: 'production',
-  ROOT_API: 'ADD_PRODUCTION_URL'
+  NODE_ENV: "production",
+  ROOT_API: " https://9njclyn1l5.execute-api.us-east-1.amazonaws.com/production/api"
 }
