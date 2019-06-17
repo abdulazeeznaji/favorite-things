a2d5235a66505001b9408eb0b81e87603a3b91ad
abdulazeeznaji <abdulazeeznaji@gmail.com>
2019-06-17 16:06:31 -0400

    Update favorite actions

--- frontend/src/store/actions.type.js
+++ frontend/src/store/actions.type.js
@@ -3,6 +3,8 @@ export const FETCH_FAVORITES = "fetchFavorites";
 export const FETCH_FAVORITE = "fetchFavorite";
 export const FAVORITE_PUBLISH = "publishFavorite";
 export const FAVORITE_DELETE = "deleteFavorite";
+export const FAVORITE_RESET_STATE = "resetFavoriteState";
+export const FAVORITE_EDIT = "editFavorite";
 
 
 // Categories actions
